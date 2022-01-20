1285a870be1dd6a70ef42ca30bd3667fd1d55040
el-despecto <95545805+el-despecto@users.noreply.github.com>
2022-01-20 15:29:50 +0300

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,9 @@
+TOKEN = "5026530893:AAHFC8icGDO_KhdaYAfyEgKdgThm9EF2jdc"
+keys ={
+    'Рубль': 'RUB',
+    'Евро': 'EUR',
+    'Доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD',
+}
